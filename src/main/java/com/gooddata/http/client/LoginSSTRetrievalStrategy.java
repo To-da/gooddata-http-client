cb7bd6f65a8a75505d3bfb30c39acb193c0fdcba
Martin Caslavsky <mcaslavsky@gmail.com>
2014-12-18 09:04:21 +0100

    FEATURE: ATP-1799 Completely switch to header based auth
    * introduce authHost as a field in client and send token requests there only
    * remove all stuff related to cookies - especially cookie store in context
    * SST and TT are stored as instance variables
    * locks are stored as instance variables as well
    * it breaks backward compatibility for the SSTRetrievalStrategy interface implementors, but we don't expect client code to implement it and it is done for the greater good of simpler code

--- src/main/java/com/gooddata/http/client/LoginSSTRetrievalStrategy.java
+++ src/main/java/com/gooddata/http/client/LoginSSTRetrievalStrategy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2013, GoodData(R) Corporation. All rights reserved.
+ * Copyright (C) 2007-2014, GoodData(R) Corporation. All rights reserved.
  * This program is made available under the terms of the BSD License.
  */
 package com.gooddata.http.client;
@@ -7,24 +7,16 @@ package com.gooddata.http.client;
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.cookie.Cookie;
-import org.apache.http.cookie.CookieOrigin;
-import org.apache.http.cookie.CookieSpec;
-import org.apache.http.cookie.MalformedCookieException;
-import org.apache.http.cookie.SM;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.cookie.BestMatchSpec;
 
 import java.io.IOException;
-import java.util.List;
 
 import static org.apache.commons.lang.Validate.notNull;
 
@@ -35,6 +27,9 @@ public class LoginSSTRetrievalStrategy implements SSTRetrievalStrategy {
 
     public static final String LOGIN_URL = "/gdc/account/login";
 
+    /** SST and TT must be present in the HTTP header. */
+    private static final int VERIFICATION_LEVEL = 2;
+
     private final Log log = LogFactory.getLog(getClass());
 
     private final String login;
@@ -43,32 +38,43 @@ public class LoginSSTRetrievalStrategy implements SSTRetrievalStrategy {
 
     private final HttpHost httpHost;
 
-    private final HttpClient httpClient;
-
     /**
      * Construct object.
+     * @deprecated Use {@link #LoginSSTRetrievalStrategy(String, String)}}
      * @param httpClient HTTP client
      * @param httpHost http host
      * @param login user login
      * @param password user password
      */
+    @Deprecated
     public LoginSSTRetrievalStrategy(final HttpClient httpClient, final HttpHost httpHost, final String login, final String password) {
-        notNull(httpClient, "HTTP Client cannot be null");
-        notNull(httpHost, "HTTP host cannot be null");
         notNull(login, "Login cannot be null");
         notNull(password, "Password cannot be null");
         this.login = login;
         this.password = password;
         this.httpHost = httpHost;
-        this.httpClient = httpClient;
+    }
+
+    /**
+     * Construct object.
+     * @param login user login
+     * @param password user password
+     */
+    @SuppressWarnings("deprecated")
+    public LoginSSTRetrievalStrategy(final String login, final String password) {
+        this(null, null, login, password);
+    }
+
+    HttpHost getHttpHost() {
+        return httpHost;
     }
 
     @Override
-    public String obtainSst() throws IOException {
+    public String obtainSst(final HttpClient httpClient, final HttpHost httpHost) throws IOException {
         log.debug("Obtaining STT");
         final HttpPost postLogin = new HttpPost(LOGIN_URL);
         try {
-            final HttpEntity requestEntity = new StringEntity(createLoginJson(login, password), ContentType.APPLICATION_JSON);
+            final HttpEntity requestEntity = new StringEntity(createLoginJson(), ContentType.APPLICATION_JSON);
             postLogin.setEntity(requestEntity);
             postLogin.setHeader("Accept", ContentType.APPLICATION_JSON.toString());
             final HttpResponse response = httpClient.execute(httpHost, postLogin);
@@ -76,34 +82,20 @@ public class LoginSSTRetrievalStrategy implements SSTRetrievalStrategy {
             if (status != HttpStatus.SC_OK) {
                 throw new GoodDataAuthException("Unable to login: " + status);
             }
-            final String sst = extractSST(response);
+            final String sst = TokenUtils.extractSST(response);
             if (sst == null) {
                 throw new GoodDataAuthException("Unable to login. Missing SST Set-Cookie header.");
             }
             return sst;
-        } catch (MalformedCookieException e) {
-            throw new GoodDataAuthException("Unable to login. Malformed Set-Cookie header.");
         } finally {
             postLogin.releaseConnection();
         }
     }
 
-    private String createLoginJson(final String login, final String password) {
+    private String createLoginJson() {
         return "{\"postUserLogin\":{\"login\":\"" + StringEscapeUtils.escapeJavaScript(login) +
-                "\",\"password\":\"" + StringEscapeUtils.escapeJavaScript(password) + "\",\"remember\":0}}";
+                "\",\"password\":\"" + StringEscapeUtils.escapeJavaScript(password) + "\",\"remember\":0" +
+                ",\"verify_level\":" + VERIFICATION_LEVEL + "}}";
     }
 
-    private String extractSST(final HttpResponse response) throws MalformedCookieException {
-        String sst = null;
-        final CookieSpec cookieSpec = new BestMatchSpec();
-        final CookieOrigin cookieOrigin = new CookieOrigin(httpHost.getHostName(), httpHost.getPort(), "/gdc/account", true);
-        for (Header header : response.getHeaders(SM.SET_COOKIE)) {
-            final List<Cookie> cookies = cookieSpec.parse(header, cookieOrigin);
-            if (cookies.size() > 0 && CookieUtils.SST_COOKIE_NAME.equals(cookies.get(0).getName())) {
-                sst = cookies.get(0).getValue();
-                break;
-            }
-        }
-        return sst;
-    }
 }
